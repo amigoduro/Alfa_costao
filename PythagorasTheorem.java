a59702a3bcd938d19048c7ee32022230bf71b11a
Cadet Adarsh <pal.adarsh10205@gmail.com>
2025-11-02 00:24:22 +0530

    Fix switch statement formatting in PythagorasTheorem.java

--- PythagorasTheorem.java
+++ PythagorasTheorem.java
@@ -30,7 +30,8 @@ public class PythagorasTheorem {
         System.out.print("Enter your choice: ");
         int choice = sc.nextInt();
 
-        switch (choice) {
+        switch (choice)
+                       {
             case 1:
                 System.out.print("Enter side a: ");                  // syclopatenic
                 double a = sc.nextDouble();
