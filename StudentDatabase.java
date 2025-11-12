463d5cf3b4c5bd4553d639ad45ed4ce8d1a4095c
Cadet Adarsh <pal.adarsh10205@gmail.com>
2025-11-12 20:46:03 +0530

    Update StudentDatabase.java

--- StudentDatabase.java
+++ StudentDatabase.java
@@ -1,6 +1,6 @@
 //header files
 
-import java.sql.Connection;        // connection header file
+import java.sql.Connection;                    // connection header file
 import java.sql.DriverManager;    // use for sql injectioning
 import java.sql.PreparedStatement;        // use for assignation 
 import java.util.Scanner;        //common header file 
