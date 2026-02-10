5f063ebfd30a87e37d209fdce115c6e1fe02d332
Cadet Adarsh <pal.adarsh10205@gmail.com>
2026-02-10 19:44:30 +0530

    Add import statement for Scanner class

--- StudentDatabase.java
+++ StudentDatabase.java
@@ -7,6 +7,7 @@ import java.util.Scanner;                   //common header file
  
 public class StudentDatabase     
                             {
+                              
     
     public static void main(String[] args) {
         
