c5d71066c7fc5e04040fcc2efe68735b4068ee55
Cadet Adarsh <pal.adarsh10205@gmail.com>
2026-02-26 21:37:13 +0530

    Update PythagorasTheorem.java

--- PythagorasTheorem.java
+++ PythagorasTheorem.java
@@ -9,7 +9,7 @@ public class PythagorasTheorem
     static double calculateHypotenuse(double a, double b)                        // paramaters
     
                                                     {
-
+ 
                                                         
         return Math.sqrt((a * a) + (b * b));
                                                       
